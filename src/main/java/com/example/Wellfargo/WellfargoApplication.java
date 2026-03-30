d74b967eb87b6e4c59732543712d87703844ceca
Initial commit - Wells Fargo project <battujojo29@gmail.com>
2026-03-30 13:51:08 +0530

    Initial commit - Wells Fargo project

---
+++ src/main/java/com/example/Wellfargo/WellfargoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wellfargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellfargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellfargoApplication.class, args);
+	}
+
+}
